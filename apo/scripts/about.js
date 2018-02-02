03f64a5685eb0a60ec19b5868bc342c2ee8f7fbf
chaoseternai <jamesho.AHS@gmail.com>
2018-02-02 02:41:44 -0800

    Completed APO nav, added same features to personal website

--- apo/scripts/about.js
+++ apo/scripts/about.js
@@ -3,3 +3,15 @@ $(".menu").click(function() {
     $(".menu-items").toggleClass("nav-expanded").css("display","");
   });
 });
+
+// This is a quickfix for the dropdown menu for mobile devices,
+// in particular for Safari because of how the browser processes
+// hover. Allows the rest of the screen to be clicked and thus hiding
+// the dropdown content.
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  $('.container, footer').click(function(e){
+    if($(".nav-expanded").css("display") == "block") {
+      $(".menu-items").toggleClass("nav-expanded").css("display","");
+    }
+  });
+}

a72fb8333f2ef21b6967c9e564f8f8f140032fe1
chaoseternai <jamesho.AHS@gmail.com>
2018-03-08 11:21:17 -0800

    Removed APO directory to separate repository

--- apo/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-// This is a quickfix for the dropdown menu for mobile devices,
-// in particular for Safari because of how the browser processes
-// hover. Allows the rest of the screen to be clicked and thus hiding
-// the dropdown content.
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('.bg-photo, .logo-container, .logo, footer').click(function(e){});
-}
